293e70010b6b73385cbaee2559c1443ad0829c5a
alixon997 <alixonibragimov55@gmail.com>
2025-07-20 11:37:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//let sum = 0;
+//for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+//console.log(sum);
+
+//for (let i = 0; i < 5; i++) {
+// console.log(fruits[i]);
+//}
+
+//const fruits = ["apple", "banana", "cherry", "ananas"];
+//const repeatCount = 3;
+//let repeatedArray = [];
+//for (let i = 0; i < repeatCount; i++) {
+    //for (let j = 0; j = fruits.length; j++){
+    //repeatedArray.push(fruits[i]);
+//}
+//}
+//console.log(repeatedArray);
+
+let i = 0;
+while (i < 5) {
+    console.log("i =", i);
+    i++;
+}
